cca9a85eab9b349ecb69b3b29ce76102ba98cb90
Krithi s shetty10024 <krithishetty146@gmail.com>
2026-02-04 19:30:00 +0530

    Initial portfolio commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  document.querySelector(".ux").style.width = "85%";
+  document.querySelector(".web").style.width = "80%";
+  document.querySelector(".ml").style.width = "65%";
+  document.querySelector(".lead").style.width = "90%";
+});
